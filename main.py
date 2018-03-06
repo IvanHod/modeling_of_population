7c77919e45ccd5a6c5141070793dd71a0e396c65
ivan <Vandamkakdam1@gmail.com>
2018-03-06 22:58:55 +0300

    added a calculation of female factor

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from data_helper import DataHelper
 
 
 class Main:
-
 	data_helper = None
 	data = {}
 	factors = {}
+	female_factor = {'general': 0, 'male': 0, 'female': 0}
 	factors_by_year = {}
-	femail_factor = None
-	relaction_male_vs_female = None
 
 	def __init__(self, country='Russian Federation', years=None):
+		if years and len(years) != 2:
+			raise Exception("Must be two years...")
+
 		if years is None:
 			years = [2000, 2005]
 
@@ -34,15 +35,50 @@ class Main:
 
 	def detect_factors(self):
 		log.info('Detect of factors...')
-		self.factors = {}
+		factors = {}
+		step = 4
+		for rn in range(0, 101, 5):
+			next_rn = rn + 5
+			interval = '{}-{}'.format(rn, rn + step)
+			next_interval = '{}-{}'.format(next_rn, next_rn + step)
+			prev_year = self.data[self.years[0]][interval]
+			if prev_year:
+				if next_interval in self.data[self.years[1]]:
+					next_year = self.data[self.years[1]][next_interval]
+					prev_year_number = prev_year['male'] + prev_year['female']
+					next_year_number = next_year['male'] + next_year['female']
+					factors[interval] = next_year_number / prev_year_number
+			else:
+				raise Exception('There is not interval {} for an year {}'.format(interval, self.years[0]))
+		self.factors = factors
 
-	def detect_femail_factor(self):
-		log.info('Detect of femail factor...')
-		self.femail_factor = None
+	def detect_female_factor(self):
+		log.info('Detect of female factor...')
+		step = 4
+		ranges = range(20, 40, 5)
+		count_woman = 0
+		for rn in ranges:
+			interval = '{}-{}'.format(rn, rn + step)
+			count_woman += self.data[self.years[0]][interval]['female']
+
+		children_interval = self.data[self.years[0]]['0-4']
+		count_children = children_interval['female'] + children_interval['male']
+
+		self.female_factor['general'] = count_children / count_woman
+
+		self.detect_relation_male_vs_female()
 
 	def detect_relation_male_vs_female(self):
-		log.info('Detect a relation birthday between male and femail...')
-		self.relaction_male_vs_female = None
+		log.info('Detect a relation birthday between male and female...')
+
+		male_coefficient = 0
+		for year in self.years:
+			interval = self.data[year]['0-4']
+			male_coefficient += interval['male'] / (interval['male'] + interval['female'])
+
+		male_coefficient /= 2
+		self.female_factor['male'] = self.female_factor['general'] * male_coefficient
+		self.female_factor['female'] = self.female_factor['general'] - self.female_factor['male']
 
 	def split_factors_by_year(self):
 		log.info('Translate factors by a year step...')
@@ -58,5 +94,6 @@ if __name__ == '__main__':
 	main.read_data()
 
 	main.detect_factors()
+	main.detect_female_factor()
 
 	sys.exit()
